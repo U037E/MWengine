468ca4b3cd5d2b802f763f18e02a7e459de1b037
Phil <42957916+U037E@users.noreply.github.com>
2019-03-14 11:21:44 +0100

    Create window.java
    starting with the window class, firstly importing every libary that seems to be necessary

---
+++ window.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mwengine;
+
+import java.awt.Canvas;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Phil
+ */
+    public class Window 
+    {
+        private JFrame frame;
+        private BufferedImage image;
+        private BufferStrategy strategy;
+        private Graphics gw;
+        private Canvas canvas;
+        
+        public Window(engineContainer ec)
+        {
+        
+        }
+    }
